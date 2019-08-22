69d4e13ed2c492ae4b0cc434a5fc059fcff311b3
Maurice <Mauricegdg@gmail.com>
2019-08-22 18:05:48 +0200

    Run changes

--- ShopBasket.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ ShopBasket.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "9828b572-84a0-423a-8a10-095e178639bc",
+		"XAMARIN_BUILD_ID", "cbccc5c4-17f0-496e-a8cb-31228b132bdc",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
